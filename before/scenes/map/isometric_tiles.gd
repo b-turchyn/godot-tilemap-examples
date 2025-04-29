7795fe6f4b46521899bb72666f416a847461fd3a
Brian Turchyn <brian@dj-bri-t.net>
2025-04-28 19:25:59 -0500

    Update for Hexagonal Tile tutorial

--- before/scenes/map/isometric_tiles.gd
+++ before/scenes/map/isometric_tiles.gd
@@ -3,12 +3,9 @@ extends Node2D
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 
 func _ready() -> void:
-	#call_deferred("test")
 	test()
-	pass
 
 func test() -> void:
 	for y in 4:
 		for x in 4:
 			tile_map_layer.set_cell(Vector2(x, y), 1, Vector2(x % 4, y % 2))
-			tile_map_layer.local_to_map()
